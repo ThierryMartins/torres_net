96458306c709d8b04c69184b938a86993fedbbc6
William Lago <williamlago2006@gmail.com>
2025-04-18 02:01:09 -0300

    Criando o JavaScript das funcoes de acessibilidade/falta implementar em outras páginas e fazer as devidas correções

--- src/acessibilidade.js
+++ src/acessibilidade.js
@@ -20,3 +20,42 @@ btn.addEventListener("click", () =>{
   });
 
 // <i class="fa-regular fa-sun"></i> -> Link do sol
+
+document.getElementById('trocar_tema').addEventListener('click',
+  () =>{
+    document.body.classList.toggle('dark')
+    if(document.body.classList.contains('dark')){
+      document.getElementById('trocar_tema').innerHTML = '<i class="fa-regular fa-sun"></i> Tema Claro'
+    }else{
+      document.getElementById('trocar_tema').innerHTML = '<i class="fa-regular fa-moon"></i> Tema Escuro'
+    }
+  }
+) 
+
+document.getElementById('tema_contraste').addEventListener('click',
+  ()=>{
+    document.body.classList.toggle('contraste')
+  }
+)
+
+document.getElementById('Tamanho_Fonte').addEventListener('click',
+  ()=>{
+    document.body.classList.toggle('more-letter')
+    if(document.body.classList.contains('more-letter')){
+      document.getElementById('Tamanho_Fonte').innerHTML = '<i class="fa fa-font fa-1x"></i> Diminuir Tamanho da Fonte'
+    }else{
+       document.getElementById('Tamanho_Fonte').innerHTML = '<i class="fa fa-font fa-2x"></i> Aumentar Tamanho da Fonte'
+    }
+  }
+)
+
+document.getElementById('link_sublinhado').addEventListener('click',
+  ()=>{
+    document.body.classList.toggle('sublinhar')
+    if(document.body.classList.contains('more-letter')){
+      document.getElementById('sublinhar').innerHTML = '<i class="fas fa-link"></i>  Retirar Links Sublinhados'
+    }else{
+      document.getElementById('sublinhar').innerHTML = '<i class="fas fa-link"></i>  Links Sublinhados'
+    }
+  }
+)
