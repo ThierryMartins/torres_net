95deb4a4cd87ac9037e289c63e9cc462e5354933
William Lago <williamlago2006@gmail.com>
2025-04-29 23:17:07 -0300

    Fazendo uma alteração no javascript do link sublinhado

--- src/acessibilidade.js
+++ src/acessibilidade.js
@@ -53,9 +53,9 @@ document.getElementById('link_sublinhado').addEventListener('click',
   ()=>{
     document.body.classList.toggle('sublinhar')
     if(document.body.classList.contains('sublinhar')){
-      document.getElementById('sublinhar').innerHTML = '<i class="fas fa-link"></i>  Retirar Links Sublinhados'
+      document.getElementById('link_sublinhado').innerHTML = '<i class="fas fa-link"></i>  Retirar Links Sublinhados'
     }else{
-      document.getElementById('sublinhar').innerHTML = '<i class="fas fa-link"></i>  Links Sublinhados'
-    }
-  }
+      document.getElementById('link_sublinhado').innerHTML = '<i class="fas fa-link"></i>  Links Sublinhados'
+    }
+  }
 )
